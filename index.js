5b20cd47bf38733ac0a865f71b0697498791fd9f
C Mendi <cesarmendezbeltran@yahoo.com.mx>
2019-02-08 20:52:42 -0600

    Calculadora con varios

---
+++ index.js
@@ -0,0 +1,7 @@
+const  math = require ('./math'); //Estoy llamando a la función en math.js que esta como modulo
+// atrás indique la ruta.
+
+console.log(math.suma(10,20));
+console.log(math.resta(10,20));
+console.log(math.multi(10,20));
+console.log(math.divide(10,20));
---
+++ math.js
@@ -0,0 +1,18 @@
+function suma (a,b) {
+  return a + b;
+}
+
+module.exports = {
+  suma: function (a,b) {
+    return a+b;
+  },
+  resta: function (a,b) {
+    return a-b;
+  },
+  multi: function (a,b) {
+    return a*b;
+  },
+  divide: function (a,b) {
+    return a/b;
+  }
+}
